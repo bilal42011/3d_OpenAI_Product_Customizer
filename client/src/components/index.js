cc2785ef9e36b7fb634e0aa7a59ff507d9da905a
Bilal Malik <bilalmalik42011@gmail.com>
2023-07-13 00:39:40 +0500

    client file moved to server

--- client/src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as CustomButton } from "./CustomButton";
-export { default as AIPicker } from "./AIPicker";
-export { default as ColorPicker } from "./ColorPicker";
-export { default as FilePicker } from "./FilePicker";
-export { default as Tab } from "./Tab";
