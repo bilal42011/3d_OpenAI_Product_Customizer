738a3652792661c090725b9a8f74646b67ab45a2
Bilal Malik <bilalmalik42011@gmail.com>
2023-07-13 01:40:11 +0500

    OpenAI issue fixed for generating images

--- server/index.js
+++ server/index.js
@@ -2,6 +2,12 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 import dalleRoutes from "./routes/dalle.routes.js";
+import path from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 dotenv.config();
 
@@ -12,9 +18,16 @@ app.use(express.json({ limit: "50mb" }));
 
 app.use("/api/v1/dalle", dalleRoutes);
 
-app.get("/", (req, res) => {
-    res.status(200).json({ message: "Hello from Dall.E " });
-});
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.resolve(__dirname, 'client', 'dist')));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'), function (err) {
+            if (err) {
+                res.status(500).send(err)
+            }
+        });
+    })
+}
 
 app.listen(8080, () => {
     console.log("Server started at port 8080");
